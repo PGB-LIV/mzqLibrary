3e347707d057a00f4b3d1f6c9b84be9ef6d98503
ukqida@gmail.com <ukqida@gmail.com@92bb3ff7-7528-5706-7235-796a0d87be8c>
2014-10-16 15:06:38 +0000

    - fixed the bug in ExtendedFeature by overriding all the getters
    Former-commit-id: e3a102d6207dc3d15fd1cb641103d923c8491061

--- src/main/java/uk/ac/liv/mzqlib/idmapper/data/ExtendedFeature.java
+++ src/main/java/uk/ac/liv/mzqlib/idmapper/data/ExtendedFeature.java
@@ -1,12 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package uk.ac.liv.mzqlib.idmapper.data;
 
 import java.util.List;
+import uk.ac.liv.jmzqml.model.mzqml.AbstractParam;
+import uk.ac.liv.jmzqml.model.mzqml.CvParam;
 import uk.ac.liv.jmzqml.model.mzqml.Feature;
+import uk.ac.liv.jmzqml.model.mzqml.RawFile;
+import uk.ac.liv.jmzqml.model.mzqml.UserParam;
 
 /**
  *
@@ -16,10 +15,10 @@ import uk.ac.liv.jmzqml.model.mzqml.Feature;
  */
 public class ExtendedFeature extends Feature {
 
-    private double lmz; // left boundary of mz
-    private double rmz; // right boundary of mz
-    private double urt; // up boundary of rt (min)
-    private double brt; // bottom boundary of rt (min)
+    private final double lmz; // left boundary of mz
+    private final double rmz; // right boundary of mz
+    private final double urt; // up boundary of rt (min)
+    private final double brt; // bottom boundary of rt (min)
     private final Feature feature;
 
     public ExtendedFeature(Feature ft) {
@@ -34,7 +33,8 @@ public class ExtendedFeature extends Feature {
             urt = (Double.valueOf(ft.getRt()) + 10.0) / 60.0;
             //urt = Double.valueOf(ft.getRt()) + 10;
             rmz = ft.getMz() + 0.05;
-        } else {
+        }
+        else {
             brt = massT.get(0);
             lmz = massT.get(1);
             urt = massT.get(2);
@@ -56,6 +56,66 @@ public class ExtendedFeature extends Feature {
 
     public double getBRT() {
         return brt;
+    }
+
+    @Override
+    public String getCharge() {
+        return feature.getCharge();
+    }
+
+    @Override
+    public RawFile getRawFile() {
+        return feature.getRawFile();
+    }
+
+    @Override
+    public String getId() {
+        return feature.getId();
+    }
+
+    @Override
+    public List<Double> getMassTrace() {
+        return feature.getMassTrace();
+    }
+
+    @Override
+    public List<AbstractParam> getParamGroup() {
+        return feature.getParamGroup();
+    }
+
+    @Override
+    public String getRt() {
+        return feature.getRt();
+    }
+
+    @Override
+    public double getMz() {
+        return feature.getMz();
+    }
+
+    @Override
+    public String getChromatogramRefs() {
+        return feature.getChromatogramRefs();
+    }
+
+    @Override
+    public String getSpectrumRefs() {
+        return feature.getSpectrumRefs();
+    }
+
+    @Override
+    public String getRawFileRef() {
+        return feature.getRawFileRef();
+    }
+
+    @Override
+    public List<CvParam> getCvParam() {
+        return feature.getCvParam();
+    }
+
+    @Override
+    public List<UserParam> getUserParam() {
+        return feature.getUserParam();
     }
 
 }

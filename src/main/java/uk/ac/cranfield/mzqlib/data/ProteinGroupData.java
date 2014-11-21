94843a6ad515ea4e86da5a59a4efb990a5d1d85b
ukqida@gmail.com <ukqida@gmail.com@92bb3ff7-7528-5706-7235-796a0d87be8c>
2014-11-21 16:28:34 +0000

    - added missing file for revision 116
    Former-commit-id: b2baf9846c4683bc2c325d00d63a2bad8878912c

---
+++ src/main/java/uk/ac/cranfield/mzqlib/data/ProteinGroupData.java
@@ -0,0 +1,45 @@
+package uk.ac.cranfield.mzqlib.data;
+
+import java.util.List;
+import uk.ac.cranfield.mzqlib.MzqLib;
+import uk.ac.liv.jmzqml.model.mzqml.ProteinGroup;
+import uk.ac.liv.jmzqml.model.mzqml.ProteinRef;
+
+/**
+ *
+ * @author Jun
+ */
+public class ProteinGroupData extends QuantitationLevel{
+    private ProteinGroup pg;
+    private final static String SEPARATOR = ";";
+    public ProteinGroupData (ProteinGroup proteinGroup){
+        pg = proteinGroup;
+    }
+
+//    public ProteinGroup getPg() {
+//        return pg;
+//    }
+
+    public String getId(){
+        return pg.getId();
+    }
+    
+    public String getAnchorProteinStr(){
+        ProteinRef lead = pg.getProteinRef().get(0);//ProteinRef 1:n
+        return lead.getProteinRef();
+    }
+    
+    public String getAmbiguityMemberStr(){
+        List<ProteinRef> proteinRefs = pg.getProteinRef();
+        if (proteinRefs.size()==1) return "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < proteinRefs.size(); i++) {
+            ProteinRef ref = proteinRefs.get(i);
+            ProteinData protein = MzqLib.data.getProtein(ref.getProteinRef());
+            sb.append(protein.getAccession());
+            sb.append(SEPARATOR);
+        }
+        sb.deleteCharAt(sb.length()-1);
+        return sb.toString();
+    }
+}
